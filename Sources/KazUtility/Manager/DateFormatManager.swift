307bf2bcb7ef3419748f0fcd997182b74d02eb9f
Taeyoung Won <hexter1994@gmail.com>
2024-02-14 21:47:17 +0900

    [Fix] 메서드 용도별로 익스텐션 분리, yyyyMMdd 포맷 추가

--- Sources/KazUtility/Manager/DateFormatManager.swift
+++ Sources/KazUtility/Manager/DateFormatManager.swift
@@ -6,31 +6,9 @@ public final class DateFormatManager {
   private init() { }
   
   private let locale = Locale(identifier: "ko_KR")
+  private let timezone = TimeZone(identifier: "ko_KR")
   private lazy var formatter = DateFormatter().configured { $0.locale = locale }
   private lazy var calendar = Calendar.current.configured { $0.locale = locale }
-  
-  public func unixTimestampToString(with interval: TimeInterval, format: Format) -> String {
-    formatter.dateFormat = format.format
-    formatter.timeZone = .current
-    
-    return formatter.string(from: Date(timeIntervalSince1970: interval))
-  }
-  
-  public func isDate(with interval: TimeInterval, by component: Calendar.Component, equalTo: Int) -> Bool {
-    return calendar.component(component, from: Date(timeIntervalSince1970: interval)) == equalTo
-  }
-  
-  public func isDate(with date: Date, by component: Calendar.Component, equalTo: Int) -> Bool {
-    return calendar.component(component, from: date) == equalTo
-  }
-  
-  public func isDate(with date: Date, by component: Calendar.Component..., equalTo: Int...) -> Bool {
-    let min = min(component.count, equalTo.count)
-    
-    return !(0..<min)
-      .map { calendar.component(component[$0], from: date) == equalTo[$0] }
-      .contains(false)
-  }
 }
 
 public extension DateFormatManager {
@@ -39,9 +17,48 @@ public extension DateFormatManager {
     case HHmm = "HH:mm"
     case HHhour = "HH시"
     case EEEE = "EEEE"
+    case yyyyMMdd = "yyyyMMdd"
     
     var format: String {
       return self.rawValue
     }
+  }
+}
+
+// MARK: - To String
+public extension DateFormatManager {
+  
+  func toString(with date: Date, format: Format) -> String {
+    formatter.dateFormat = format.format
+    formatter.timeZone = timezone
+    
+    return formatter.string(from: date)
+  }
+  
+  func unixTimestampToString(with interval: TimeInterval, format: Format) -> String {
+    formatter.dateFormat = format.format
+    formatter.timeZone = .current
+    
+    return formatter.string(from: Date(timeIntervalSince1970: interval))
+  }
+}
+
+// MARK: - Compare Date
+public extension DateFormatManager {
+  
+  func isDate(with interval: TimeInterval, by component: Calendar.Component, equalTo: Int) -> Bool {
+    return calendar.component(component, from: Date(timeIntervalSince1970: interval)) == equalTo
+  }
+  
+  func isDate(with date: Date, by component: Calendar.Component, equalTo: Int) -> Bool {
+    return calendar.component(component, from: date) == equalTo
+  }
+  
+  func isDate(with date: Date, by component: Calendar.Component..., equalTo: Int...) -> Bool {
+    let min = min(component.count, equalTo.count)
+    
+    return !(0..<min)
+      .map { calendar.component(component[$0], from: date) == equalTo[$0] }
+      .contains(false)
   }
 }
