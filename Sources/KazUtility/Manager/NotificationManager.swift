4926c42bb7a033ed4b6a9f7116f5e004ecd94242
Taeyoung Won <hexter1994@gmail.com>
2024-02-15 18:36:01 +0900

    [Fix] Key 타입 associated 선언

--- Sources/KazUtility/Manager/NotificationManager.swift
+++ Sources/KazUtility/Manager/NotificationManager.swift
@@ -73,7 +73,9 @@ public extension NotificationInfoKey {
 }
 
 public protocol NotificationInfo {
-  var key: any NotificationInfoKey { get }
+  associatedtype Key: NotificationInfoKey
+  
+  var key: Key { get }
   var value: Any { get }
 }
 
