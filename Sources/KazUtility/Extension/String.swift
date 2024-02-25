e244e9bc93cd7157197625e72ac001b8f7db891a
Taeyoung Won <hexter1994@gmail.com>
2024-02-25 23:43:53 +0900

    [Add] String 익스텐션 추가

---
+++ Sources/KazUtility/Extension/String.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+public extension String {
+  
+  static func combineWithLineBreaks(_ strings: String...) -> Self {
+    return strings.joined(separator: "\n")
+  }
+  
+  var asMarkdownRedneredAttributeString: NSAttributedString? {
+    guard let data = self.data(using: .utf8) else {
+      return nil
+    }
+    
+    let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+      .documentType: NSAttributedString.DocumentType.html,
+      .characterEncoding: String.Encoding.utf8.rawValue
+    ]
+    
+    return try? NSAttributedString(data: data, options: options, documentAttributes: nil)
+  }
+  
+  func colorAttributed(rangeText: String, color: UIColor) -> NSAttributedString {
+    let attributedString = NSMutableAttributedString(string: self)
+    
+    attributedString.addAttribute(
+      .foregroundColor,
+      value: color,
+      range: (self as NSString).range(of: rangeText)
+    )
+    
+    return attributedString
+  }
+}
