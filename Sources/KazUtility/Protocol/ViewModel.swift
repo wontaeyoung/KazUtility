fc055f85998e814ed90984a787f36c18e9a073d1
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 09:46:36 +0900

    [Add] Input Output 타입 추가

--- Sources/KazUtility/Protocol/ViewModel.swift
+++ Sources/KazUtility/Protocol/ViewModel.swift
@@ -1,5 +1,10 @@
 public protocol ViewModel: AnyObject {
+  
   associatedtype CoordinatorType: Coordinator
+  associatedtype Input
+  associatedtype Output
   
   var coordinator: CoordinatorType? { get set }
+  
+  func transform(input: Input) -> Output
 }
