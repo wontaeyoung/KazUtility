fd325b10c5fc99317e82445f0522c22f0be9ac5b
Taeyoung Won <hexter1994@gmail.com>
2024-02-28 20:14:10 +0900

    [Fix] 공통 에러를 CommonError로 변경, LogManager 로깅 로직 수정

--- Sources/KazUtility/Manager/LogManager.swift
+++ Sources/KazUtility/Manager/LogManager.swift
@@ -24,7 +24,11 @@ public final class LogManager {
   private lazy var networkLogger = Logger(subsystem: LogCategory.bundleID, category: LogCategory.network.category)
   private lazy var localLogger = Logger(subsystem: LogCategory.bundleID, category: LogCategory.local.category)
   
-  public func log(with error: AppError, to logTarget: LogCategory, level: OSLogType = .error) {
+  public func log(with error: Error, to logTarget: LogCategory, level: OSLogType = .error) {
+    guard let error = error as? AppError else {
+      return log(with: error, to: .local)
+    }
+    
     let message = error.logDescription
         
     switch logTarget {
